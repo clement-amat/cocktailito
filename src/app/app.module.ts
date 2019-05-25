84b44ec7b178fa4f2b641d5de6e662e1399e944e
Clément A <clemamat@gmail.com>
2019-05-26 00:36:47 +0200

    slideshow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import {RouterModule} from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { DetailComponent } from './detail/detail.component';
+import { SliderComponent } from './slider/slider.component';
+
+const routes = [
+  {path: 'details', component: DetailComponent},
+  {path: '**', component: HomeComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
